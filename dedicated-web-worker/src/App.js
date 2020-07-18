a34ab8b60f0b9877ed1072037451a4d35203fa58
Salil Kapoor <49902237+salilkapoor@users.noreply.github.com>
2020-07-18 22:35:53 +0530

    Titile changed

--- dedicated-web-worker/src/App.js
+++ dedicated-web-worker/src/App.js
@@ -1,11 +1,10 @@
-import React from "react";
-import "./styles.css";
+import React from 'react';
+import './styles.css';
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Dedicated Web Worker</h1>
     </div>
   );
 }
