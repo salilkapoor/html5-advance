ed90ab4bb56941f92531b0ad081bb87a50ce90a7
Salil Kapoor <49902237+salilkapoor@users.noreply.github.com>
2020-07-21 01:11:39 +0530

    feat(indexedDB): added indexed DB

--- indexed-db/src/App.js
+++ indexed-db/src/App.js
@@ -1,11 +1,24 @@
-import React from "react";
-import "./styles.css";
+import React, { useEffect } from 'react'
+
+import * as fromDB from './utils/idb'
+
+import './styles.css'
 
 export default function App() {
+  fromDB.openDB()
+  useEffect(() => {
+    async function fetchPost() {
+      const data = await fetch('https://jsonplaceholder.typicode.com/posts/1')
+      const postOne = await data.json()
+      return postOne
+    }
+    fetchPost().then(res => fromDB.addPost(res))
+  }, [])
+  fromDB.getPost()
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Indexed DB Sample</h1>
+      <div>{fromDB.getPost()}</div>
     </div>
-  );
+  )
 }
---
+++ indexed-db/src/utils/idb.js
@@ -0,0 +1,62 @@
+function _OpenDB() {
+  return indexedDB.open('post-store', 1)
+}
+
+export function openDB() {
+  let openRequest = _OpenDB()
+  openRequest.onupgradeneeded = function() {
+    let db = openRequest.result
+    if (!db.objectStoreNames.contains('post')) {
+      // if there's no "books" store
+      db.createObjectStore('post', { keyPath: 'id' }) // create it
+    }
+    // triggers if the client had no database
+    // ...perform initialization...
+  }
+
+  openRequest.onerror = function() {
+    console.error('Error', openRequest.error)
+  }
+
+  openRequest.onsuccess = function(postObj) {}
+}
+
+export function addPost(postObj) {
+  let openRequest = _OpenDB()
+  openRequest.onsuccess = function() {
+    let db = openRequest.result
+    let transaction = db.transaction('post', 'readwrite')
+    let post = transaction.objectStore('post')
+
+    let request = post.add(postObj)
+
+    request.onsuccess = function() {
+      // (4)
+      console.log('Post added to the store', request.result)
+    }
+
+    request.onerror = function() {
+      console.log('Error', request.error)
+    }
+  }
+}
+
+export function getPost() {
+  let openRequest = _OpenDB()
+  openRequest.onsuccess = function() {
+    let db = openRequest.result
+    let transaction = db.transaction('post', 'readonly')
+    let post = transaction.objectStore('post')
+
+    let request = post.getAll()
+    console.log(request)
+    request.onsuccess = function() {
+      // (4)
+      console.log('get from the store', request.result)
+    }
+
+    request.onerror = function() {
+      console.log('Error', request.error)
+    }
+  }
+}
